6f1fd58e0e04787c5a4a3a7213bcb5eb1b29fead
Pratiti Basu <104024345+coderpanda02@users.noreply.github.com>
2023-04-27 23:08:53 +0530

    Update CallBy_Value_Object.java

--- Java_Constructors_Methods/src/methods/CallBy_Value_Object.java
+++ Java_Constructors_Methods/src/methods/CallBy_Value_Object.java
@@ -3,21 +3,16 @@ package methods;
 //	There are 2 types of method call ->
 //
 //	   1. Call By Value : Passing a copy of the variable.
-//						  -> It doesn't change the actual value.
-//	   2. Call By Reference (or Object) : Passing a reference of the object   
-//							(i.e the variable through which the object can be accessed).
-//							 -> It changes the actual value.
+//			    -> It doesn't change the actual value.
+//	   2. Call By Reference (or Object) : Passing a reference of the object (i.e the variable through which the object can be accessed).
+//					    -> It changes the actual value.
 //
-//	The parameters passed to method are called Actual parameters and the parameters
-//	received by method are called Formal parameters.
+//	The parameters passed to method are called Actual parameters and the parameters received by method are called Formal parameters.
 //
-//	In case of Call by value, values of actual parameter are copied to the formal 
-//	parameter where both the parameters are stored in different memory locations.  
-//	So, any changes made inside the method being called are not reflected in actual 
-//	parameters of the caller method.
+//	In case of Call by value, values of actual parameter are copied to the formal parameter where both the parameters are stored in different memory locations.  
+//	So, any changes made inside the method being called are not reflected in actual parameters of the caller method.
 //
-//	But, for Call by reference, both actual and formal parameters refer to the same
-//	memory location. So, any changes made inside the method being called are 
+//	But, for Call by reference, both actual and formal parameters refer to the same memory location. So, any changes made inside the method being called are 
 //	actually reflected in the actual parameters of the caller method.
 
 
