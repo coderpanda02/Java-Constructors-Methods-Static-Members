7e3422d9323c4f4fb508d9a93d706e92b7c4d6ea
Pratiti Basu <104024345+coderpanda02@users.noreply.github.com>
2023-04-27 23:15:17 +0530

    Update Static1_Usage.java

--- Java_Constructors_Methods/src/static_keyword/Static1_Usage.java
+++ Java_Constructors_Methods/src/static_keyword/Static1_Usage.java
@@ -1,16 +1,15 @@
 package static_keyword;
 
-//	Static members are class related, not dependent on objects. The value for static
-//	members remain throughout the whole program. They can be accessed anywhere within
-//	the class and no object creation is needed to call the static members. 
+//	Static members are class related, not dependent on objects. The value for static members remain throughout the whole program. They can be accessed
+//	anywhere within the class and no object creation is needed to call the static members. 
 //
 //	There are static members like, static block, variable or method.
-//	The memory allocation for static variables and methods are done in the Linking 
-//	process of the ClassLoader. Static variables are initialized and static blocks are
-//	executed in the next step, i.e. Initialization process of the ClassLoader.
 //
-//	Java main method is static as main is the first method to be executed following  
-//	the static block (if any present) in a program before the object creation.
+//	The memory allocation for static variables and methods are done in the Linking process of the ClassLoader. Static variables are initialized and 
+//	static blocks are executed in the next step, i.e. Initialization process of the ClassLoader.
+//
+//	Java main method is static as main is the first method to be executed following the static block (if any present) in a program before the object creation.	 
+
 
 
 public class Static1_Usage {
@@ -34,8 +33,7 @@ public class Static1_Usage {
 		
 		Static1_Usage obj = new Static1_Usage();
 		
-//		static int i = 0; //gives error - illegal modifiers, as when initializing 
-//		static variable inside static block or method, no need to add 'static',
+//		static int i = 0; //gives error - illegal modifiers, as when initializing static variable inside static block or method, no need to add 'static',
 //		initializing outside the static block, have to add 'static'
 	
 		
@@ -49,8 +47,8 @@ public class Static1_Usage {
 			i++;
 			
 //			System.out.println("value of instance variable j = "+j); //gives error -
-			//Cannot make a static reference to the non-static field j, when j is
-			//defined outside the main method, have to call it by the instance, obj.j
+			//Cannot make a static reference to the non-static field j, when j is defined outside the main method,
+			//have to call it by the instance, obj.j
 			
 			System.out.println("value of instance variable j = "+obj.j);
 			obj.j++;
